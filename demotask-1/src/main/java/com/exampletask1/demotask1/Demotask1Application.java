e9ef2bea9f2d90012e8236a4ec9565ccb92d0f5e
sannith36 <sannithreddy36@gmail.com>
2023-10-17 11:23:52 -0400

    intial commit

---
+++ demotask-1/src/main/java/com/exampletask1/demotask1/Demotask1Application.java
@@ -0,0 +1,13 @@
+package com.exampletask1.demotask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotask1Application.class, args);
+	}
+
+}
---
+++ demotask-1/src/test/java/com/exampletask1/demotask1/Demotask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampletask1.demotask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demotask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
